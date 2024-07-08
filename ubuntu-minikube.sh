76432a79084adab37643cac41e462bde15f7b98a
Rajasekharreddy Penugonda <142084632+rajasekharreddy4545@users.noreply.github.com>
2024-07-08 10:52:46 +0530

    Update ubuntu-minikube.sh

--- ubuntu-minikube.sh
+++ ubuntu-minikube.sh
@@ -11,4 +11,6 @@ sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/
 sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo kubectl version --client
+sudo kubectl version --client --output=yaml
 sudo minikube start --driver=docker --force
